5ba0430d18850c4806b30972394430e01781e539
Bill0806 <56011480+Bill0806@users.noreply.github.com>
2019-10-08 15:26:48 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,431 @@
+            .header {
+                font-weight: 400;
+                /* 文字粗細 */
+                position: relative;
+                /* 定位 */
+                z-index: 5000;
+                /* Z 方向位置，z-index 數字越大的在越上面，反之則在越下面。 */
+                background-color:#fff;
+                /* 背景顏色 */
+                height: 54px;
+                /* DIV高度設定*/
+            }
+            .header-logo {
+                position: absolute;
+                top: 0;
+                left:40%;
+                right:40%;
+                margin: 0 80px;
+                /* div 邊界間距 */
+            }
+            .header-element {
+            position: relative;
+            z-index: 5001;
+            }
+            .header-btn {
+            display: inline-block;
+            /* 類似float 但不需要做clear = http://zh-tw.learnlayout.com/inline-block.html
+            可以設定width 與 height、margin 屬性 */
+            line-height: 50px;
+            vertical-align: top;
+            /* 讓圖片靠上對齊 */
+            padding: 0 5px;
+            /* 控制區域如 DIV 或 span 的內部距離，也可以控制 HTML 的表格內部距離（例如文字或圖片與邊框的距離）
+            ，padding 本身就是內距的設定，padding 是不可以設定負值 */
+            cursor: pointer;
+            /* 改變滑鼠游標 */
+            }
+            .background {
+            width: 100%;
+            height:500px;
+            background-size: cover;
+            background-position: center;
+            background-image: url(https://dgo9uctxwg500.cloudfront.net/images/2334x980/2019_index_banner_menberday_back_f9d00273ab.png);
+            }
+            
+            .dYkNeS {
+            margin: 0 auto;
+            max-width: 410px;
+            width: 100%;
+            margin-right: auto;
+            padding: 0;
+           
+            }   
+            .ioyCcs {
+            display: -webkit-box;
+            display: -webkit-flex;
+            display: -ms-flexbox;
+            display: flex;
+            
+            }
+            .dCFvjw {
+            flex: 1;
+            padding: 14px 0;
+            line-height: 22px;
+            text-align: center;
+            background-color: #ffffff;
+            color: #000000;
+            border-bottom: solid 1px #c1c0c1;
+            cursor: pointer;
+            margin-top: 50px;
+            }
+            .ezDsyh {
+            flex: 1;
+            padding: 14px 0;
+            line-height: 22px;
+            text-align: center;
+            background-color: #ffffff;
+            color: #000000;
+            border-bottom: solid 1px #c1c0c1;
+            cursor: pointer;
+            margin-top: 50px;
+            }
+            .jWvGNy {
+            background-color: #ffffff;
+            width:410px;
+            height: 330px;
+            margin-left:739px
+            }
+            .pig{
+                height:100px ;
+                width: 300px;  
+                color: #4A90E2;
+                margin-left:50px;
+                padding-top:250px;
+            }
+            .kpnSKI {
+            background-color: #f04e2f;
+            border-radius: 4px;
+            /* 使四周圓角化 */
+            color: #ffffff;
+            padding: 15px 0;
+            text-align: center;
+            cursor: pointer;
+            
+            }
+
+            .sc-eh {
+            position: relative;
+            overflow: hidden;
+            /* 内容修剪，並且内容不可见，须有一个指定的高度（height或者max-height）
+            或者将white-space设置为nowra，*/
+            max-width: 1200px;
+            margin-left: 315px;
+            padding-bottom: 30px;
+            /* https://www.1keydata.com/css-tutorial/tw//padding.php
+            可以設定留白，分別為長度、百分比、以及 'auto */
+            
+            }
+            .sc-eh1 {
+                padding-left:90px;
+                margin-bottom: 20px;
+            }
+            .sc-eh1-1 {
+                display: inline-block;
+                width: 220px;
+                height: 320px;
+                margin: 1em;
+                box-shadow: 0px 6px 6px 0px #eee;
+            }
+            .sc-eh1-1-1{
+                display: inline-block;
+                overflow: hidden
+                max-width: 300px;
+                
+            }
+            .ctiUpw {
+            width: 100%;
+            white-space: nowrap;
+            overflow-x: auto;
+            overflow-y: hidden;
+            -webkit-overflow-scrolling: touch;
+            margin-bottom: 50px;
+            text-align: center;
+            }
+            .hSNUMP {
+            margin-top:15px;
+            margin-bottom: 25px;
+            margin-left:75px;
+            overflow-y:auto;
+            }
+            .dXZViI {
+            border-bottom: 1px solid #eee;
+            padding-bottom: 10px;
+            float:left;
+            width: 50%;
+            margin-bottom: 20px;
+            }
+            .fBgQiN {
+            display: inline-block;
+            width: 45%;
+            vertical-align: middle;
+            text-align: center;
+            }
+            .eHCnzF {
+            width: 180px;
+            height: 60px;
+            
+            background-image: url(https://dgo9uctxwg500.cloudfront.net/images/480x480%23/zh_TW_e7ce295363.png);
+            background-size: cover;
+            background-position: center;
+            }
+            .aa {
+            width: 180px;
+            height: 60px;
+            
+            background-image: url(https://dgo9uctxwg500.cloudfront.net/images/480x480%23/zh_TW_36357b6c3b.png);
+            background-size: cover;
+            background-position: center;
+            }
+            .bb {
+            width: 180px;
+            height: 60px;
+            
+            background-image: url(https://dgo9uctxwg500.cloudfront.net/images/480x480%23/zh_TW_6cf5f15e14.png);
+            background-size: cover;
+            background-position: center;
+            }
+            .cc {
+            width: 180px;
+            height: 60px;
+            
+            background-image: url(https://dgo9uctxwg500.cloudfront.net/images/480x480%23/zh_TW_887e1148f8.png);
+            background-size: cover;
+            background-position: center;
+            }
+            .dd {
+            width: 180px;
+            height: 60px;
+            
+            background-image: url(https://dgo9uctxwg500.cloudfront.net/images/480x480%23/zh_TW_a6546f203b.png);
+            background-size: cover;
+            background-position: center;
+            } .ee {
+            width: 180px;
+            height: 60px;
+            
+            background-image: url(https://dgo9uctxwg500.cloudfront.net/images/480x480%23/zh_TW_4bc779a76a.png);
+            background-size: cover;
+            background-position: center;
+            } .ff {
+            width: 180px;
+            height: 60px;
+            
+            background-image: url(https://dgo9uctxwg500.cloudfront.net/images/480x480%23/zh_TW_887e1148f8.png);
+            background-size: cover;
+            background-position: center;
+            } .gg {
+            width: 180px;
+            height: 60px;
+            
+            background-image: url(https://dgo9uctxwg500.cloudfront.net/images/480x480%23/zh_TW_90d1767b74.png);
+            background-size: cover;
+            background-position: center;
+            }
+            .enTicl {
+                display: inline-block;
+            font-size:18px;
+            vertical-align: middle;
+            text-align: right;
+            color:gray;
+            padding-left: 7px;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            white-space: nowrap;
+            margin-left:0px;
+            }   
+            .tile-img {
+            padding: 0 10px;
+            width:200px ;
+            background-size: cover;
+            background-position: center;
+            background-color: #fff;
+            }
+            .tile-subtitle {
+            font-size: 14px;
+            color:rgb(66, 62, 62);
+            }
+            .tile-fans {
+            font-size: 6px;
+            color:gray;
+            }
+            .tile-info {
+            -webkit-box-sizing: border-box;
+             box-sizing: border-box;
+             background-color: #fff;
+             padding: 10px 6px 6px;
+              height: 90px;
+            }
+            .fDkSBO {
+            font-weight: 500;
+            font-size: 18px;
+            padding: 3px 0 3px 16px;
+            margin-bottom: 18px;
+            color: #666;
+            border-top: 1px solid #666;
+            border-bottom: 1px solid #666;
+            /* border-style：邊框樣式
+            top: 上框線 bottom: 下框線 left: 左框線 right: 右框線 border: 四邊框線
+            width: 邊線的寬度 color：邊線的顏色 */
+            text-align: left;
+            display: none;
+            }
+            .tildh {
+            margin-top:20px ;
+            padding: 2px ;
+            text-align:center;
+            margin-bottom: 15px;
+            font-size: 20px;
+            color:gray;
+            }
+            .efyHwW {
+            position: relative;
+            overflow: hidden;
+            max-width: 960px;
+            margin-left: auto;
+            margin-right: auto;
+            margin-bottom: 30px;
+            padding-bottom: 30px;
+            padding: 20px;
+            margin: 0 auto 20px;
+            text-align: center
+            }
+            .knJikE {
+            font-size: 18px;
+            }
+            .dBDqHS {
+            padding: 0 25px 25px;
+            }
+            .edsQPR {
+            margin-bottom: 8px;
+            line-height: 1.6;
+            }
+            .row {
+            margin-left: -8px;
+            margin-right: -8px;
+            
+            }
+            .col {
+             width: 60%;
+             display:inline-block;
+             width: 400px;
+             text-align:left;
+             margin-bottom: 15px;
+            }
+            .shortlink-tile {
+            background: #fff;
+            display: inline-block;
+            padding-bottom: 18px;
+            text-align:left;
+            color: #222;
+           
+            }       
+            .link-icon {
+            width: 10px;
+            height: 5px;
+            margin-left: 8px;
+            margin-bottom:20px; 
+            }
+            .link-title {
+            line-height: 30px;
+            font-weight: 300;
+            }
+            .jWHloL {
+            font-size: 18px;
+            }
+            .bvqFEX {
+            background: url(https://d1gpbxqmt7wq2i.cloudfront.net/asset/mobile/images/pattern_bg.png),#f0beb2;
+            height: 80px;
+            font-size: 18px;
+            padding: 25px 15px;
+            text-align: center;
+            }
+            .tile-text {
+            font-size: 20px;
+            color: #f04e2f;
+            margin-bottom: 15px;
+            }
+            .tile-text02 {
+            display: inline-block;
+            color: #ffffff;
+            background-color: #f04e2f;
+            padding: 8px 50px;
+            border-radius: 30px;
+            cursor: pointer;
+            }
+            .footer {
+             height: 250px;
+             background: url(https://d1gpbxqmt7wq2i.cloudfront.net/asset/mobile/images/pattern_bg.png), #333;
+             color: #999;
+             font-size: 14px;
+             overflow: hidden;
+             max-height: 500px;
+             opacity: 1.95;
+            }
+            
+            .footer-wrapper {
+            position: relative;
+            max-width: 960px;
+            margin: 0 auto;
+            padding: 0 15px;
+            }
+            .footer-nav {
+            display: block;
+            }
+            .lang-selector-wrap {
+            margin-top:25px;
+            float: right;
+            width: 180px;
+            }
+            
+            .lang-selector {
+            -webkit-appearance: none;
+            border: 0;
+            border-radius: 0;
+            padding: 10px 24px 10px 54px;
+            background-color:rgba(138, 133, 133, 0.623) ;
+            color:rgb(255, 255, 255);
+            font-size: 16px;
+            line-height: 18px;
+            outline: none;
+            width: 100%;
+            cursor: pointer;
+            }
+            option {
+            font-weight: normal;
+            display: block;
+            white-space: pre;
+            min-height: 1.2em;
+            padding: 0px 2px 1px;
+            }
+            .footer-navlist {
+            text-align: left;
+            }
+            .footer-navlist li {
+            display: inline-block;
+            }
+            .navlink {
+            display: inline-block;
+            padding: 10px 8px;
+            color: #fff;
+            line-height: 20px;
+            font-size:14px;
+            }
+            .footer-content {
+            text-align: center;
+            line-height: 2;
+            border-top: 1px solid #666666;
+            padding-top: 20px;
+            }
+            .social-panel {
+            margin: 20px 0;
+            }
+            a {
+            color: #4A90E2;
+            text-decoration: none;
+            }
+            ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+            }
